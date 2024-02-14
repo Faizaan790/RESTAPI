af5bd7fe573e3f40e57dc54fb48c23379bd8b7aa
Mohammed Faizaan Khan <faizaanhaque329@gmail.com>
2024-02-13 18:12:52 -0600

    First Commit

--- src/main/java/com/restapi/task_manager_api/TaskManagerApiApplication.java
+++ src/main/java/com/restapi/task_manager_api/TaskManagerApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskManagerApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApiApplication.class, args);
     }
